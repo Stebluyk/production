beb3973caac37d13532f902607c370dc044e8d10
Stebluyk <dimon0896@mail.ru>
2017-05-22 17:09:31 +0300

    Update countryInfo.sh

--- test_task/countryInfo.sh
+++ test_task/countryInfo.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-
-/etc/init.d/postgresql start;
-sleep 40;
-
-echo "Hello $(whoami)"
-if [ "$(whoami)" = 'root' ] || [ "$(whoami)" = 'postgres' ]; then
-        if [ $1 = '' ]; then
-                psql application -c 'select id as num, nicename, iso3 as iso, numcode, phonecode from country'
-        else
-                psql application -c "select id as num, nicename, iso3 as iso, numcode, phonecode from country where iso3 = '$1'"
-        fi
+if [$1 = '']; then
+        sudo -u postgres psql -d application -c "SELECT id as num, nicename as name, iso3 as iso, numcode, phonecode FROM country;"
 else
-        echo "Plesae be root or postgres to run this script correctly"
+        sudo -u postgres psql -d application -c "SELECT id as num, nicename as name, iso3 as iso, numcode, phonecode FROM country WHERE iso3='$1';"
 fi
 
